72ad6c6301b353a95c1f599c8537c95656464018
Christofferson Kyle Garcia <rikuuuuu6@gmail.com>
2024-11-13 14:32:33 +0800

    first commit

---
+++ GARCIA_LAB11.py
@@ -0,0 +1,46 @@
+
+
+grades = []
+student_count = 0
+
+
+while student_count < 5:
+        if student_count == 0:
+            number = "1st"
+        elif student_count == 1:
+            number = "2nd"
+        elif student_count == 2:
+            number = "3rd"   
+        elif student_count == 3:
+            number = "4th"   
+        elif student_count == 4:
+            number = "5th"   
+                
+        grade = float(input(f"Enter grade for {number} student: "))
+            
+        if 40 <= grade <= 100:
+            grades.append(grade)
+            student_count +=1
+        else:
+            print("Grade must be between 40 and 100")
+                            
+average_grade = sum(grades) / len(grades)
+
+print ("================================")
+
+passing_grades = 0 
+for grade in grades:
+    if grade >= 75: 
+        passing_grades += 1
+        
+
+passing_percentage = (passing_grades / len(grades)) * 100
+
+print("Grades: ", grades)
+print(f"Average Grade: ", average_grade)
+print(f"Passing Grade: ", passing_grades)
+print(f"Passing Percentage: ", passing_percentage, "%")
+
+
+    
+    
---
+++ GARCIA_LAB11_STRING.py
@@ -0,0 +1,36 @@
+word_list = []
+print("Enter 10 words (press Enter after each word):")
+
+print("===========================")
+
+while len(word_list) < 10:  
+    word = input("Enter word: ")
+    
+    if len(word_list) >= 10 and word.lower() == 'done': 
+        break
+        
+    if word: 
+        word_list.append(word)
+        
+    if len(word_list) < 10:  
+        print(f"Please enter {10 - len(word_list)} more words")
+
+min_length = int(input("Enter a number: "))
+
+print("===========================")
+
+found_words = []
+for word in word_list:
+    if len(word) >= min_length:
+        found_words.append(word)
+        continue
+    print(f"{word} is only {len(word)} letters - too short")
+
+print("===========================")
+
+if found_words:
+    print(f"Words with {min_length} or more letters:")
+    for word in found_words:
+        print(f"{word} - {len(word)} letters")
+else:
+    print(f"No words with {min_length} or more letters found")
